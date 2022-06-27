31cce68c53d1d4e6269588c74dc3b5c2a4933aad
Alyssa Ivana <alyssaiv@oa-g2.msk.21-school.ru>
2022-06-27 19:57:15 +0300

    add conv int to dec

--- Convertational/s21_from_int_to_decimal.c
+++ Convertational/s21_from_int_to_decimal.c
@@ -7,5 +7,13 @@
 #include "../s21_decimal.h"
 
 int s21_from_int_to_decimal(int src, s21_decimal *dst) {
-    
+    int error = 0;
+    if (src >= 0) {
+        dst->bits[0] = src;
+    } else {
+        src = -src;
+        dst->bits[0] = src;
+        s21_set_Sign(dst);
+    }
+    return error;
 }
