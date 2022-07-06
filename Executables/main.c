256383808da2bf159bb5457faef5b179575927c3
Joramun Sasin <joramuns@student.21-school.ru>
2022-07-06 22:38:48 +0300

    Main function with mul_ten tests

--- Executables/main.c
+++ Executables/main.c
@@ -13,25 +13,27 @@ int main(int argc, const char * argv[]) {
     s21_dec_zero(&res);
     //
     val1.LOWBIT = 123;
-    val1.MEDBIT = 432;
+    val1.MEDBIT = 0;
     val1.HIGHBIT = 0;
     val1.bits[3] = 0;
-    s21_set_sign(&val1);
+//    s21_set_sign(&val1);
     //
     val2.LOWBIT = 123000;
     val2.MEDBIT = 798;
     val2.HIGHBIT = 0;
     val2.bits[3] = 0;
 //    s21_set_sign(&val2);
-    s21_print_dec_long(res);
+//    s21_print_dec_long(res);
     printf("..\n");
     s21_print_decimal(val1);
-    printf("-\n");
-    s21_print_decimal(val2);
-    s21_add(val1, val2, &res);
+    printf("*10\n");
+//    s21_print_decimal(val2);
+// 123 -> mul ten by 27 is overflow
+    s21_mul_ten(&val1, 27);
     printf("=\n");
 //    s21_print_dec_long(res);
-    s21_print_decimal(res);
+//    s21_print_decimal(res);
+    s21_print_decimal(val1);
 
     return 0;
 }
