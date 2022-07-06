5a97e3bf4438dd9f4b096baf3dbb1e1e3ba5a1f2
Joramun Sasin <joramuns@student.21-school.ru>
2022-07-06 20:00:31 +0300

    CPPCHECK

--- Comparative/s21_is_greater.c
+++ Comparative/s21_is_greater.c
@@ -12,7 +12,8 @@ int s21_is_greater(s21_decimal value_1, s21_decimal value_2) {
     }
     for (int i = 95; i > -1 && result == 2; i--) {
         if (s21_get_bit_long(value_1, i) != s21_get_bit_long(value_2, i)) {
-            result = s21_get_sign(value_1) == 0 ? s21_get_bit_long(value_1, i) : !s21_get_bit_long(value_1, i);
+            result = s21_get_sign(value_1) == 0 ? \
+            s21_get_bit_long(value_1, i) : !s21_get_bit_long(value_1, i);
         }
     }
     if (result == 2) {
