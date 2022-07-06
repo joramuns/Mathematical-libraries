c23526a09c9cc42429d71eb59c5bd3b11a37c501
Joramun Sasin <joramuns@student.21-school.ru>
2022-07-06 20:00:21 +0300

    s21_add different signs handled, s21_sub refactored

--- Arithmetic/s21_add.c
+++ Arithmetic/s21_add.c
@@ -5,16 +5,33 @@
 
 #include "../s21_decimal.h"
 
-int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+int s21_sum_dec(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
     int ex_code = 0;
-    int len = (s21_last_non_zero(value_1) >= s21_last_non_zero(value_2)) ? s21_last_non_zero(value_1) : s21_last_non_zero(value_2);
+    int len = (s21_last_non_zero(value_1) >= s21_last_non_zero(value_2)) ? \
+    s21_last_non_zero(value_1) : s21_last_non_zero(value_2);
 
     for (int i = 0; (i <= len) && !ex_code; i++) {
         ex_code = s21_sum_bit(i, result, s21_get_bit_long(value_1, i));
-        ex_code = s21_sum_bit(i, result, s21_get_bit_long(value_2, i));
+        if (!ex_code) ex_code = s21_sum_bit(i, result, s21_get_bit_long(value_2, i));
     }
 
     return ex_code;
 }
 
+int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+    int sign_1 = s21_get_sign(value_1), sign_2 = s21_get_sign(value_2);
+    int ex_code = 0;
 
+    s21_zero_bit(&value_1, 127);
+    s21_zero_bit(&value_2, 127);
+    if ((!sign_1 && !sign_2) || (sign_1 && sign_2)) {
+        ex_code = s21_sum_dec(value_1, value_2, result);
+        if (sign_1 && sign_2) s21_set_sign(result);
+    } else if (sign_1) {
+        s21_sub(value_2, value_1, result);
+    } else if (sign_2) {
+        s21_sub(value_1, value_2, result);
+    }
+
+    return ex_code;
+}
--- Arithmetic/s21_sub.c
+++ Arithmetic/s21_sub.c
@@ -21,29 +21,26 @@ int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
     } else if (!sign_1 && !sign_2) {
         if (s21_is_equal(value_1, value_2)) {
             s21_dec_zero(result);
-        } else if (s21_is_greater(value_1, value_2)) {
-            s21_set_sign(result);
-            s21_invert_bit_long(&value_2);
-            s21_set_bit(result, 0);
-            s21_add(value_1, value_2, result);
-            s21_zero_bit(result, 127); // NEGATE
         } else {
-            s21_set_sign(result);
-            s21_invert_bit_long(&value_1);
             s21_set_bit(result, 0);
-            s21_add(value_2, value_1, result);
+            if (s21_is_greater(value_1, value_2)) {
+                s21_invert_bit_long(&value_2);
+                s21_add(value_1, value_2, result);
+            } else {
+                s21_invert_bit_long(&value_1);
+                s21_add(value_2, value_1, result);
+                s21_set_sign(result);
+            }
         }
     } else if (sign_1) {
-        s21_zero_bit(&value_1, 127); // NEGATE
+        s21_zero_bit(&value_1, 127);
         s21_sub(value_1, value_2, result);
         s21_set_sign(result);
     } else if (sign_2) {
-        s21_zero_bit(&value_2, 127); // NEGATE
+        s21_zero_bit(&value_2, 127);
         s21_sub(value_1, value_2, result);
         s21_set_sign(result);
     }
-
-    
 
     return 0;
 }
