8b1feacd419f042b61febd052f081b0af9077279
Joramun Sasin <joramuns@student.21-school.ru>
2022-07-07 15:39:19 +0300

    Division by ten start development

---
+++ Supportive/s21_div_ten.c
@@ -0,0 +1,12 @@
+//
+//  s21_div_ten.c
+//  Decimal
+//
+
+#include "s21_decimal.h"
+
+int s21_div_ten(s21_decimal *value, int scale) {
+    int ex_code = 0;
+
+    return ex_code;
+}
--- s21_decimal.h
+++ s21_decimal.h
@@ -36,8 +36,9 @@ void    s21_zero_bit(s21_decimal *decimal, int bit_index);
 void    s21_set_inf(s21_decimal *result);
 int     s21_sum_bit(int i, s21_decimal *result, int add_bit);
 void    s21_dec_zero(s21_decimal *decimal);
-int    s21_mul_ten(s21_decimal *value, int scale);
+int     s21_mul_ten(s21_decimal *value, int scale);
 void    s21_dec_copy(s21_decimal src, s21_decimal *dest);
+int     s21_div_ten(s21_decimal *value, int scale);
 
 /*                      Simple arithmetic operations                            */
 /* Return codes:                                                                */
