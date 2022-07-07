d9af2363254552877178013af0a79bbc9d4e2c03
Joramun Sasin <joramuns@student.21-school.ru>
2022-07-07 15:22:12 +0300

    Another version of mul_ten

--- Supportive/s21_mul_ten.c
+++ Supportive/s21_mul_ten.c
@@ -7,7 +7,7 @@
 
 int s21_mul_ten(s21_decimal *value, int scale) {
     int sign = 0, ex_code = 0;
-    s21_decimal mul_2, mul_8;
+    s21_decimal mul_2, mul_8, temp;
 
     if (s21_get_sign(*value)) {
         s21_zero_bit(value, 127);
@@ -15,12 +15,19 @@ int s21_mul_ten(s21_decimal *value, int scale) {
     }
     while (scale-- > 0 && !ex_code) {
         if (s21_last_non_zero(*value) < 93) {
+//            s21_dec_copy(*value, &mul_2);
+//            s21_dec_copy(*value, &mul_8);
+//            s21_dec_zero(value);
+//            s21_left_shift_bit(&mul_2, 1);
+//            s21_left_shift_bit(&mul_8, 3);
+//            s21_add(mul_2, mul_8, value);
+//            (n << 2 + n) << 1 ?
+            s21_dec_copy(*value, &temp);
             s21_dec_copy(*value, &mul_2);
-            s21_dec_copy(*value, &mul_8);
             s21_dec_zero(value);
-            s21_left_shift_bit(&mul_2, 1);
-            s21_left_shift_bit(&mul_8, 3);
-            s21_add(mul_2, mul_8, value);
+            s21_left_shift_bit(&temp, 2);
+            s21_add(temp, mul_2, value);
+            s21_left_shift_bit(value, 1);
         } else {
             ex_code = 1;
             s21_set_inf(value);
