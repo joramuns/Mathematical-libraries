0028e57670573ef1f63cda50341ac4bd4e453b22
Joramun Sasin <joramuns@student.21-school.ru>
2022-07-07 15:09:22 +0300

    Right shift implemented

--- Supportive/s21_shift_bit.c
+++ Supportive/s21_shift_bit.c
@@ -23,11 +23,11 @@ int s21_left_shift_bit(s21_decimal *decimal, int step) {
 int s21_right_shift_bit(s21_decimal *decimal, int step) {
     int temp[3] = {0, 0, 0};
     for (; step > 0; step--) {
-        for (int i = 0; i < 3; i++) {
-            temp[i] = s21_get_bit(decimal->bits[i], 31);
-            decimal->bits[i] <<= 1;
-            if (i > 0 && temp[i - 1]) {
-                s21_set_bit(decimal, i * 32);
+        for (int i = 2; i >= 0; i--) {
+            temp[i] = s21_get_bit(decimal->bits[i], 0);
+            decimal->bits[i] >>= 1;
+            if (i < 2 && temp[i + 1]) {
+                s21_set_bit(decimal, i * 32 + 31);
             }
         }
     }
