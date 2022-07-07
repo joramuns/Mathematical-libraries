50f1853a7ea309084205dbe21fee21232167569f
Joramun Sasin <joramuns@student.21-school.ru>
2022-07-07 22:06:33 +0300

    New version of mul

--- Arithmetic/s21_mul.c
+++ Arithmetic/s21_mul.c
@@ -6,23 +6,17 @@
 #include "../s21_decimal.h"
 
 int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
-    s21_decimal mul_dec, temp;
-    s21_dec_copy(value_2, &mul_dec);
-    int len = s21_last_non_zero(value_1);
+    s21_decimal zero, temp;
+    s21_dec_zero(&zero);
     int ex_code = 0;
-
-    for (int i = 0; i <= len && !ex_code; i++) {
-        if (s21_get_bit_long(value_1, i)) {
+    while (s21_is_not_equal(value_2, zero) && !ex_code) {
+        if (s21_get_bit_long(value_2, 0)) {
             s21_dec_copy(*result, &temp);
             s21_dec_zero(result);
-            ex_code = s21_add(temp, mul_dec, result);
+            ex_code = s21_add(temp, value_1, result);
         }
-        if (!s21_get_bit_long(mul_dec, 95)) {
-            s21_left_shift_bit(&mul_dec, 1);
-        } else {
-            ex_code = 1;
-            s21_set_inf(result);
-        }
+        s21_left_shift_bit(&value_1, 1);
+        s21_right_shift_bit(&value_2, 1);
     }
 
     return ex_code;
