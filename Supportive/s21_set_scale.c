39781e9f7ca858772a06f33adbcec6beaa07bcd1
Joramun Sasin <joramuns@student.21-school.ru>
2022-07-14 17:12:12 +0300

    New function to set scale

--- Supportive/s21_set_scale.c
+++ Supportive/s21_set_scale.c
@@ -2,7 +2,18 @@
 //  s21_set_scale.c
 //  Decimal
 //
-//  Created by Joramun Sasin on 7/14/22.
-//
 
-#include <stdio.h>
+#include "s21_decimal.h"
+
+int s21_set_scale(s21_decimal *decimal, int scale) {
+    int ex_code = 0;
+
+    if (scale < 29) {
+        scale <<= 16;
+        decimal->bits[3] |= scale;
+    } else {
+        ex_code = 1;
+    }
+
+    return ex_code;
+}
