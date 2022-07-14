c43a62051bcc6488ee09874f371cfe89b6ca7be7
Joramun Sasin <joramuns@student.21-school.ru>
2022-07-14 17:10:56 +0300

    Multiply scale preparation, overflow handling

--- Arithmetic/s21_mul.c
+++ Arithmetic/s21_mul.c
@@ -8,15 +8,26 @@
 int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
     s21_decimal zero, temp;
     s21_dec_zero(&zero);
-    int ex_code = 0;
+    int ex_code = 0;//, scale_1 = s21_get_scale(value_1), scale_2 = s21_get_scale(value_2);
+//    int dif_scale = scale_1 + scale_2;
+//    if (dif_scale > 28) {
+//        dif_scale -= 28;
+//        s21_div_ten(&value_1, dif_scale);
+//        s21_div_ten(&value_2, dif_scale);
+//    }
     while (s21_is_not_equal(value_2, zero) && !ex_code) {
         if (s21_get_bit_long(value_2, 0)) {
             s21_dec_copy(*result, &temp);
             s21_dec_zero(result);
             ex_code = s21_add(temp, value_1, result);
         }
-        s21_left_shift_bit(&value_1, 1);
-        s21_right_shift_bit(&value_2, 1);
+        if (s21_last_non_zero(value_1) < 95) {
+            s21_left_shift_bit(&value_1, 1);
+            s21_right_shift_bit(&value_2, 1);
+        } else {
+            ex_code = 1;
+            s21_set_inf(result);
+        }
     }
 
     return ex_code;
---
+++ Supportive/s21_set_scale.c
@@ -0,0 +1,8 @@
+//
+//  s21_set_scale.c
+//  Decimal
+//
+//  Created by Joramun Sasin on 7/14/22.
+//
+
+#include <stdio.h>
