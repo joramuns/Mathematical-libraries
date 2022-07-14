7a9cf9f7b90fda697f1655c455dcec8fa644e352
Joramun Sasin <joramuns@student.21-school.ru>
2022-07-14 17:13:01 +0300

    Refactoring get scale function to reduce complications

--- Supportive/s21_get_scale.c
+++ Supportive/s21_get_scale.c
@@ -6,10 +6,10 @@
 #include "../s21_decimal.h"
 
 int s21_get_scale(s21_decimal decimal) {
-    int scale = 0;
-    for (int i = 23; i > 15; i--) {
-        scale <<= 1;
-        scale |= s21_get_bit(decimal.bits[3], i);
-    }
+    int scale = decimal.bits[3];
+
+    scale <<= 1;
+    scale >>= 17;
+
     return scale;
 }
