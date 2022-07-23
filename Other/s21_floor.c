96dd8f28b810bd2c11565ec98eefc320c3871bfc
Alyssa Ivana <alyssaiv@oa-h2.msk.21-school.ru>
2022-07-23 17:28:14 +0300

    add floor

--- Other/s21_floor.c
+++ Other/s21_floor.c
@@ -7,5 +7,16 @@
 #include "../s21_decimal.h"
 
 int s21_floor(s21_decimal value, s21_decimal *result) {
+    int sign = s21_get_sign(value);
+    if (sign) {
+        s21_truncate(value, result);
+        if (!s21_is_equal(value, *result)) {
+            s21_decimal one = {1, 0, 0, 0};
+            s21_sub(value, one, result);
+            s21_truncate(*result, result);
+        }
+    } else {
+        s21_truncate(value, result);
+    }
     return 0;
 }
