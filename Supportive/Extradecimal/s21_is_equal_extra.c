44f4ea627719ba91cc0e8fcdb248f841eb05af4c
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-04 16:26:21 +0300

    Supportive extradecimal comparative functions added

--- Supportive/Extradecimal/s21_is_equal_extra.c
+++ Supportive/Extradecimal/s21_is_equal_extra.c
@@ -5,4 +5,14 @@
 //  Created by Joramun Sasin on 8/4/22.
 //
 
-#include "s21_is_equal_extra.h"
+#include "s21_decimal_extra.h"
+
+int s21_is_equal_extra(s21_decimal_extra value_1, s21_decimal_extra value_2) {
+    int result = 1;
+
+    for (int i = 0; i < EXTRASIZE && result; i++) {
+        if (value_1.bits[i] != value_2.bits[i]) result = 0;
+    }
+
+    return result;
+}
