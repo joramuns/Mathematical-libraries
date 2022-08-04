6ed9cc7fcc697a4cb90101e26bb9b1778db9ae5e
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-04 16:25:15 +0300

    s21_sub_extra bug fixed

---
+++ Supportive/Extradecimal/s21_is_equal_extra.c
@@ -0,0 +1,8 @@
+//
+//  s21_is_equal_extra.c
+//  Decimal
+//
+//  Created by Joramun Sasin on 8/4/22.
+//
+
+#include "s21_is_equal_extra.h"
---
+++ Supportive/Extradecimal/s21_is_less_extra.c
@@ -0,0 +1,8 @@
+//
+//  s21_is_less_extra.c
+//  Decimal
+//
+//  Created by Joramun Sasin on 8/4/22.
+//
+
+#include <stdio.h>
