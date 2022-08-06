c7b09975fcec041ce2d474b3709d07a54161c9ea
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-06 20:25:41 +0300

    s21_add qualize + div_ten scale removal

--- Arithmetic/s21_add.c
+++ Arithmetic/s21_add.c
@@ -19,8 +19,11 @@ int s21_sum_dec(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
 }
 
 int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
-    int sign_1 = s21_get_sign(value_1), sign_2 = s21_get_sign(value_2);
     int ex_code = 0;
+    int sign_1 = s21_get_sign(value_1), sign_2 = s21_get_sign(value_2);
+    int scale_1 = s21_get_scale(value_1), scale_2 = s21_get_scale(value_2);
+    int dif_scale = (scale_1 > scale_2) ? scale_1 : scale_2;
+    s21_scale_equalize(&value_1, &value_2);
 
     s21_zero_bit(&value_1, 127);
     s21_zero_bit(&value_2, 127);
@@ -33,6 +36,7 @@ int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
     } else if (sign_2) {
         ex_code = s21_sub(value_1, value_2, result);
     }
+    s21_set_scale(result, dif_scale);
     if (ex_code) {
         if (s21_get_sign(*result)) ex_code = 2;
         s21_dec_zero(result);
@@ -40,3 +44,5 @@ int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
 
     return ex_code;
 }
+
+// 5.(3) + 5.(3) ??? (28 scales)
