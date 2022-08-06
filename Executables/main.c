8573c18a35ad679389cb456a37bd339c1f2f482f
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-06 16:50:27 +0300

    Main file changes

--- Executables/main.c
+++ Executables/main.c
@@ -16,30 +16,25 @@ int main(int argc, const char * argv[]) {
     val1.MEDBIT = 0xFFFFFFFF;
     val1.HIGHBIT = 0xFFFFFFFF;
     val1.bits[3] = 0;
-    s21_set_scale(&val1, 0);
-//
-    val2.LOWBIT = 11;
+    s21_set_scale(&val1, 1);
+
+    val2.LOWBIT = 1;
     val2.MEDBIT = 0;
     val2.HIGHBIT = 0;
     val2.bits[3] = 0;
-    s21_set_scale(&val2, 1);
+    s21_set_scale(&val2, 2);
 
 //    s21_set_sign(&val1);
-    s21_set_sign(&val2);
+//    s21_set_sign(&val2);
 
-//    s21_print_dec_long(res);
-//    printf("..\n");
-//    ex_code = s21_scale_equalize(&val1, &val2);
     s21_print_decimal(val1);
     printf("/\n");
     s21_print_decimal(val2);
-// 123 -> mul ten by 27 is overflow, 1 -> mul ten by 29 is overflow
-    ex_code = s21_mul(val1, val2, &res);
-//    ex_code = s21_div_ten(&val1);
+
+    ex_code = s21_div(val1, val2, &res);
+
     printf("=\n");
-//    s21_print_dec_long(res);
     s21_print_decimal(res);
-//    s21_print_decimal(val1);
 
     return ex_code;
 }
