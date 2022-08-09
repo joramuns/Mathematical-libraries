334b4865d625eedd973af221d95ba13547605a6d
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-09 22:24:48 +0300

    s21_floor fix

--- Other/s21_floor.c
+++ Other/s21_floor.c
@@ -8,15 +8,13 @@
 
 int s21_floor(s21_decimal value, s21_decimal *result) {
     int sign = s21_get_sign(value);
+
+    s21_truncate(value, result);
     if (sign) {
-        s21_truncate(value, result);
         if (!s21_is_equal(value, *result)) {
             s21_decimal one = {1, 0, 0, 0};
-            s21_sub(value, one, result);
-            s21_truncate(*result, result);
+            s21_sub(*result, one, result);
         }
-    } else {
-        s21_truncate(value, result);
     }
     return 0;
 }
