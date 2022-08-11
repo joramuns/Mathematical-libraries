0f961047b27de1605e47da9be262bab61a3e2d09
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-11 19:19:32 +0300

    feat(s21_mod): add function
    Остаток от деления 0 на число со степенью в тестах возвращает 0 со степенью делителя. Баг или фича?

--- Arithmetic/s21_mod.c
+++ Arithmetic/s21_mod.c
@@ -7,5 +7,22 @@
 #include "../s21_decimal.h"
 
 int s21_mod(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
-    return 0;
+    int ex_code = 0;
+    s21_decimal temp = INITDEC, temp2 = INITDEC;
+    s21_zero_bit(&value_1, 127);
+    s21_zero_bit(&value_2, 127);
+
+    ex_code = s21_div(value_1, value_2, &temp);
+    if (!ex_code) {
+        if (s21_is_equal(value_1, temp2)) {
+            s21_dec_copy(temp, result);
+        } else {
+            s21_truncate(temp, &temp2);
+            s21_dec_zero(&temp);
+            s21_mul(value_2, temp2, &temp);
+            s21_sub(value_1, temp, result);
+        }
+    }
+
+    return ex_code;
 }
