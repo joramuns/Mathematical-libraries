3c48e9a549739d65cebbf7dbd6e91ca6174804ce
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-14 20:42:02 +0300

    style(from_int_to_decimal): explicit library declaration

--- Convertational/s21_from_int_to_decimal.c
+++ Convertational/s21_from_int_to_decimal.c
@@ -3,7 +3,6 @@
 //  Decimal
 //
 
-#include <stdio.h>
 #include "../s21_decimal.h"
 
 int s21_from_int_to_decimal(int src, s21_decimal *dst) {
---
+++ Executables/Tests/common.c
@@ -0,0 +1,8 @@
+//
+//  common.c
+//  check_main
+//
+//  Created by Joramun Sasin on 8/14/22.
+//
+
+#include <stdio.h>
