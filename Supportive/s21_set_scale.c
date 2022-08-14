857af5103780f27bca282331e002bba626549724
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-14 15:31:58 +0300

    fix(set_scale): delete handling sub_flag

--- Supportive/s21_set_scale.c
+++ Supportive/s21_set_scale.c
@@ -8,14 +8,12 @@
 int s21_set_scale(s21_decimal *decimal, int scale) {
     int ex_code = 0;
     int sign = s21_get_sign(*decimal);
-    int sub_flag = s21_get_sub_flag(*decimal);
     decimal->bits[3] = 0;
 
     if (scale >= 0 && scale < 29) {
         scale <<= 16;
         decimal->bits[3] |= scale;
         if (sign) s21_set_sign(decimal);
-        if (sub_flag) s21_set_sub_flag(decimal);
     } else {
         ex_code = 1;
     }
