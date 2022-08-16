506f86a307b95e363d7f3b995b2247a7a599569c
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-16 21:04:52 +0300

    style(simple): add macro for double loops

--- Simple/s21_mult_number.c
+++ Simple/s21_mult_number.c
@@ -12,8 +12,8 @@ int     s21_mult_number(matrix_t *A, double number, matrix_t *result) {
 
     ex_code = s21_create_matrix(A->rows, A->columns, result);
     if (ex_code == OK) {
-        for (int i_row = 0; i_row < A->rows; i_row++) {
-            for (int i_column = 0; i_column < A->columns; i_column++) {
+        EVERY_ROW
+            EVERY_COLUMN
                 result->matrix[i_row][i_column] = A->matrix[i_row][i_column] * number;
             }
         }
