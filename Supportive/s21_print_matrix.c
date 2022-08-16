3edcc377d2500d39feea40f688e91c3cb2417e73
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-16 12:54:56 +0300

    feat(print_matrix): add print_matrix function

--- Supportive/s21_print_matrix.c
+++ Supportive/s21_print_matrix.c
@@ -5,4 +5,21 @@
 //  Created by Joramun Sasin on 8/16/22.
 //
 
-#include <stdio.h>
+#include "../s21_matrix.h"
+
+int    s21_print_matrix(matrix_t matrix) {
+    int ex_code = OK;
+
+    if (matrix.matrix) {
+        for (int i_row = 0; i_row < matrix.rows; i_row++) {
+            for (int i_column = 0; i_column < matrix.columns; i_column++) {
+                printf("[%.0f]", matrix.matrix[i_row][i_column]);
+            }
+            printf("\n");
+        }
+    } else {
+        ex_code = M_ERROR;
+    }
+
+    return ex_code;
+}
