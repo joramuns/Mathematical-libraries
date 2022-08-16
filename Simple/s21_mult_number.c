e34421d1c88a26f943065421a77fe76bb1002ec1
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-16 15:21:18 +0300

    feat(mult_number): add function that multiplies matrix with a number

--- Simple/s21_mult_number.c
+++ Simple/s21_mult_number.c
@@ -10,5 +10,14 @@
 int     s21_mult_number(matrix_t *A, double number, matrix_t *result) {
     int ex_code = OK;
 
+    ex_code = s21_create_matrix(A->rows, A->columns, result);
+    if (ex_code == OK) {
+        for (int i_row = 0; i_row < A->rows; i_row++) {
+            for (int i_column = 0; i_column < A->columns; i_column++) {
+                result->matrix[i_row][i_column] = A->matrix[i_row][i_column] * number;
+            }
+        }
+    }
+
     return ex_code;
 }
