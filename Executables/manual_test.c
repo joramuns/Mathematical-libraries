75b8693eb8d70a3dc7fb73514d60da8b640769bf
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-16 12:55:23 +0300

    chore(main): add main function for manual testing

--- Executables/manual_test.c
+++ Executables/manual_test.c
@@ -5,4 +5,15 @@
 //  Created by Joramun Sasin on 8/16/22.
 //
 
-#include <stdio.h>
+#include "../s21_matrix.h"
+
+int main() {
+    int ex_code = OK;
+
+    matrix_t val1;
+    s21_create_matrix(9, 9, &val1);
+    ex_code = s21_print_matrix(val1);
+    if (ex_code == OK) s21_remove_matrix(&val1);
+
+    return ex_code;
+}
