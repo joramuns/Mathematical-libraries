9b816f3e504c3b6a700b7c8a6886e5b2eec4c6e8
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-16 14:05:14 +0300

    feat(compare_matrix_sizes): add function to compare matrix sizes, with checking existence inside

---
+++ Supportive/s21_compare_matrix_sizes.c
@@ -0,0 +1,19 @@
+//
+//  s21_compare_matrix_sizes.c
+//  s21_matrix
+//
+//  Created by Joramun Sasin on 8/16/22.
+//
+
+#include "../s21_matrix.h"
+
+int s21_compare_matrix_sizes(matrix_t A, matrix_t B) {
+    int ex_code = s21_check_matrix(A);
+
+    if (ex_code == OK) ex_code = s21_check_matrix(B);
+    if (ex_code == OK) {
+        if ((A.rows != B.rows || A.columns != B.columns)) ex_code = M_ERROR;
+    }
+
+    return ex_code;
+}
