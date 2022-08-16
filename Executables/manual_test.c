da6a674484d79d4e25d5072d85c1f9be92545db6
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-16 15:06:50 +0300

    chore(main): improvement of manual testing unit

--- Executables/manual_test.c
+++ Executables/manual_test.c
@@ -10,10 +10,31 @@
 int main() {
     int ex_code = OK;
 
-    matrix_t val1;
-    s21_create_matrix(9, 9, &val1);
-    ex_code = s21_print_matrix(val1);
-    if (ex_code == OK) s21_remove_matrix(&val1);
+/*                                   CREATE                                     */
+    matrix_t val1, val2, res;
+    s21_create_matrix(5, 5, &val1);
+    s21_create_matrix(5, 5, &val2);
+
+
+    s21_randomize_matrix(&val1, 99);
+    s21_randomize_matrix(&val2, 99);
+
+/*                                    ACTION                                    */
+    ex_code = s21_sum_matrix(&val1, &val2, &res);
+
+/*                                    PRINT                                     */
+    s21_print_matrix(val1);
+    printf("\n");
+    s21_print_matrix(val2);
+    printf("\n");
+    s21_print_matrix(res);
+
+/*                                    CLEAN                                     */
+    s21_remove_matrix(&val1);
+    s21_remove_matrix(&val2);
+    s21_remove_matrix(&res);
 
     return ex_code;
 }
+
+
