38a6c6b47a8bc82a03cfdc86d5745659e4a05148
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-16 14:06:53 +0300

    feat(check_matrix): check pointer, check rows and columns not less than 1

--- Supportive/s21_check_matrix.c
+++ Supportive/s21_check_matrix.c
@@ -10,5 +10,13 @@
 int s21_check_matrix(matrix_t A) {
     int ex_code = OK;
 
+    if (A.matrix) {
+        if (A.columns < 1 || A.rows < 1) {
+            ex_code = M_ERROR;
+        }
+    } else {
+        ex_code = M_ERROR;
+    }
+
     return ex_code;
 }
