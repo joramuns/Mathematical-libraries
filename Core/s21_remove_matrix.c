c74edae1d4faf3ee74127d8cc7292432e1af3d26
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-16 12:54:36 +0300

    feat: add create_matrix and remove_matrix functions

--- Core/s21_remove_matrix.c
+++ Core/s21_remove_matrix.c
@@ -8,5 +8,10 @@
 #include "../s21_matrix.h"
 
 void    s21_remove_matrix(matrix_t *A) {
-
+    if (A->matrix) {
+        for (int i_row = 0; i_row < A->rows; i_row++) {
+            free(A->matrix[i_row]);
+        }
+        free(A->matrix);
+    }
 }
