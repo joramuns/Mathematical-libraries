696da1a27b344a33ec72ed89f0d3c40b48adfbda
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-16 21:08:12 +0300

    feat(calc_complements): start implementing

--- Specific/s21_calc_complements.c
+++ Specific/s21_calc_complements.c
@@ -7,8 +7,21 @@
 
 #include "../s21_matrix.h"
 
+int     s21_complements_go(matrix_t *A, matrix_t *result) {
+    int ex_code = OK;
+
+    return ex_code;
+}
+
 int     s21_calc_complements(matrix_t *A, matrix_t *result) {
     int ex_code = OK;
+
+    if (A->rows == A->columns) {
+        ex_code = s21_create_matrix(A->rows, A->columns, result);
+        if (ex_code == OK) ex_code = s21_complements_go(A, result);
+    } else {
+        ex_code = C_ERROR;
+    }
 
     return ex_code;
 }
