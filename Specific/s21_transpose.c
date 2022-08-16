c8de2f784158c01d2945466ef8312c35b3aca34a
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-16 15:40:56 +0300

    feat(transpose): add function transpose

--- Specific/s21_transpose.c
+++ Specific/s21_transpose.c
@@ -10,5 +10,14 @@
 int     s21_transpose(matrix_t *A, matrix_t *result) {
     int ex_code = OK;
 
+    ex_code = s21_create_matrix(A->columns, A->rows, result);
+    if (ex_code == OK) {
+        for (int i_row = 0; i_row < A->rows; i_row++) {
+            for (int i_column = 0; i_column < A->columns; i_column++) {
+                result->matrix[i_column][i_row] = A->matrix[i_row][i_column];
+            }
+        }
+    }
+
     return ex_code;
 }
