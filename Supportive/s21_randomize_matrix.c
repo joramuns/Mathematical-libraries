3228a622639bfc34b7a418bd4d2ed85566249b5e
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-16 21:09:44 +0300

    style: macros for loops, explicit space in function declaration

--- Supportive/s21_randomize_matrix.c
+++ Supportive/s21_randomize_matrix.c
@@ -8,7 +8,7 @@
 #include "../s21_matrix.h"
 #include <time.h>
 
-void s21_randomize_matrix (matrix_t *A, int range) {
+void s21_randomize_matrix(matrix_t *A, int range) {
     srand((unsigned)time(0));
     for (int i_row = 0; i_row < A->rows; i_row++) {
         for (int i_column = 0; i_column < A->columns; i_column++) {
