77eb4c04e74a163602c11091f40b229165095fef
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-17 13:49:11 +0300

    tests: add structure for tests

---
+++ Tests/check_eq_matrix.c
@@ -0,0 +1,20 @@
+//
+//  check_eq_matrix.c
+//  s21_matrix
+//
+//  Created by Joramun Sasin on 8/17/22.
+//
+
+#include "check_matrix.h"
+
+Suite *eq_matrix_suite(void) {
+    Suite *s = suite_create("");
+    TCase *tc = tcase_create("");
+
+    tcase_add_test(tc, one);
+
+    suite_add_tcase(s, tc);
+
+    return s;
+}
+
