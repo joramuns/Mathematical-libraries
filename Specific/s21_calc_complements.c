043e9ea00e3f16b31a1ab2752ede41d6306e68f2
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-17 13:50:39 +0300

    feat(calc_complements): finish function

--- Specific/s21_calc_complements.c
+++ Specific/s21_calc_complements.c
@@ -9,6 +9,20 @@
 
 int     s21_complements_go(matrix_t *A, matrix_t *result) {
     int ex_code = OK;
+    int sign = 1;
+
+    EVERY_ROW
+        EVERY_COLUMN
+            matrix_t temp_matrix = {0};
+            s21_create_matrix(A->rows - 1, A->columns - 1, &temp_matrix);
+            s21_determinant_cofactor(A, &temp_matrix, i_row, i_column, A->rows);
+            double det = 0.0;
+            s21_determinant(&temp_matrix, &det);
+            result->matrix[i_row][i_column] = sign * det;
+            sign = -sign;
+            s21_remove_matrix(&temp_matrix);
+        }
+    }
 
     return ex_code;
 }
