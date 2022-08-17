0f22d2b6b277f1e64ecf9145fc6f4d5e07ee62e9
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-17 20:59:09 +0300

    fix(inverse_matrix): exit code fix

--- Specific/s21_inverse_matrix.c
+++ Specific/s21_inverse_matrix.c
@@ -12,7 +12,7 @@ int     s21_inverse_matrix(matrix_t *A, matrix_t *result) {
     double det = 0.0;
 
     if (A->matrix) {
-        s21_determinant(A, &det);
+        ex_code = s21_determinant(A, &det);
     } else {
         ex_code = M_ERROR;
     }
@@ -20,16 +20,18 @@ int     s21_inverse_matrix(matrix_t *A, matrix_t *result) {
         matrix_t calc_complement = {0};
         ex_code = s21_calc_complements(A, &calc_complement);
         if (ex_code == OK) {
-            EVERY_ROW
-                EVERY_COLUMN
-                    calc_complement.matrix[i_row][i_column] /= det;
-                }
-            }
             ex_code = s21_transpose(&calc_complement, result);
             s21_remove_matrix(&calc_complement);
+            if (ex_code == OK) {
+                EVERY_ROW
+                    EVERY_COLUMN
+                        result->matrix[i_row][i_column] /= det;
+                    }
+                }
+            }
         }
-    } else {
-        ex_code = C_ERROR;
+    } else if (ex_code == OK) {
+        ex_code = M_ERROR;
     }
 
     return ex_code;
