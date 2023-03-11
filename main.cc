731cfc4b3267f253a6028c5d94851fc171b8a641
Joramun Sasin <joramuns@student.21-school.ru>
2023-03-11 19:22:57 +0300

    tests: multiplication test

--- main.cc
+++ main.cc
@@ -3,19 +3,26 @@
 #include "s21_matrix_oop.h"
 
 int main() {
-  S21Matrix c(12, 9);
-  S21Matrix a(9, 12);
-  a(9, 11) = 3;
+  S21Matrix c(5, 3);
+  for (int i = 1; i <= 5; i++) {
+    for (int j = 1; j <= 3; j++) {
+      c(i, j) = 1;
+    }
+  }
+  S21Matrix a(3, 5);
+  a(3, 5) = 3;
   S21Matrix b(a);
-  b(9, 11) = 18;
+  b(3, 1) = 18;
   a.SubMatrix(b);
   b.MulNumber(9);
   c.SumMatrix(a);
-  c.MulMatrix(a);
   std::cout << "First matrix:" << std::endl;
   a.PrintMatrix();
   std::cout << std::endl << "Second matrix:" << std::endl;
   b.PrintMatrix();
+  std::cout << std::endl << "Third matrix:" << std::endl;
+  c.PrintMatrix();
+  c.MulMatrix(a);
   std::cout << std::endl << "Third matrix:" << std::endl;
   c.PrintMatrix();
   std::cout << std::endl << "Method check: " << a.EqMatrix(b) << std::endl;
