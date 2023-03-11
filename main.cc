4f611367c4f5f2fa54baa84ddbd3f754ee66bc9c
Joramun Sasin <joramuns@student.21-school.ru>
2023-03-11 15:40:48 +0300

    tests: main with comparison test

--- main.cc
+++ main.cc
@@ -4,7 +4,14 @@
 
 int main() {
   S21Matrix a(9, 12);
-  a(9, 13) = 9;
+  S21Matrix b(9, 12);
+  a(9, 11) = 9;
+  b(9, 11) = 8;
+  std::cout << "First matrix:" << std::endl;
   a.PrintMatrix();
+  std::cout << std::endl << "Second matrix:" << std::endl;
+  b.PrintMatrix();
+  std::cout << std::endl << "Method check: " << a.EqMatrix(b) << std::endl;
+  std::cout << std::endl << "Operator check: " << (a == b) << std::endl;
   return 0;
 }
