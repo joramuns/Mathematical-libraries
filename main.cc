29fa5adb5dfc9d1d40dfa04b90ec19a8a3cf07b7
Joramun Sasin <joramuns@student.21-school.ru>
2023-03-11 18:31:38 +0300

    tests: simple operation coverage

--- main.cc
+++ main.cc
@@ -3,14 +3,21 @@
 #include "s21_matrix_oop.h"
 
 int main() {
+  S21Matrix c(12, 9);
   S21Matrix a(9, 12);
-  S21Matrix b(9, 12);
-  a(9, 11) = 9;
-  b(9, 11) = 8;
+  a(9, 11) = 3;
+  S21Matrix b(a);
+  b(9, 11) = 18;
+  a.SubMatrix(b);
+  b.MulNumber(9);
+  c.SumMatrix(a);
+  c.MulMatrix(a);
   std::cout << "First matrix:" << std::endl;
   a.PrintMatrix();
   std::cout << std::endl << "Second matrix:" << std::endl;
   b.PrintMatrix();
+  std::cout << std::endl << "Third matrix:" << std::endl;
+  c.PrintMatrix();
   std::cout << std::endl << "Method check: " << a.EqMatrix(b) << std::endl;
   std::cout << std::endl << "Operator check: " << (a == b) << std::endl;
   return 0;
