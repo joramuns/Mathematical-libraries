711ce40813a6f52cc50ec43e1372ac8f26eda2a6
Joramun Sasin <joramuns@student.21-school.ru>
2023-03-11 20:19:26 +0300

    tests: for transpose

--- main.cc
+++ main.cc
@@ -11,20 +11,27 @@ int main() {
   }
   S21Matrix a(3, 5);
   a(3, 5) = 3;
+  a(1, 3) = 8;
   S21Matrix b(a);
-  b(3, 1) = 18;
-  a.SubMatrix(b);
-  b.MulNumber(9);
-  c.SumMatrix(a);
-  std::cout << "First matrix:" << std::endl;
+  b(3, 1) = 4;
+  /* a.SubMatrix(b); */
+  /* b.MulNumber(9); */
+  /* c.SumMatrix(a); */
+  std::cout << "Matrix A:" << std::endl;
   a.PrintMatrix();
-  std::cout << std::endl << "Second matrix:" << std::endl;
+  std::cout << std::endl << "Matrix B:" << std::endl;
   b.PrintMatrix();
-  std::cout << std::endl << "Third matrix:" << std::endl;
+  std::cout << std::endl << "Matrix C:" << std::endl;
   c.PrintMatrix();
-  c.MulMatrix(a);
-  std::cout << std::endl << "Third matrix:" << std::endl;
+  c.MulMatrix(b);
+  std::cout << std::endl << "Matrix C*B:" << std::endl;
   c.PrintMatrix();
+  S21Matrix d = b.Transpose();
+  std::cout << std::endl << "Matrix D, B transposed:" << std::endl;
+  d.PrintMatrix();
+  S21Matrix e = d.Transpose();
+  std::cout << std::endl << "Matrix E, D transposed:" << std::endl;
+  e.PrintMatrix();
   std::cout << std::endl << "Method check: " << a.EqMatrix(b) << std::endl;
   std::cout << std::endl << "Operator check: " << (a == b) << std::endl;
   return 0;
