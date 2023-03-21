b1342f39125673406d448d1ad89b009dedeae9f3
Joramun Sasin <joramuns@student.21-school.ru>
2023-03-21 21:56:19 +0300

    tests: colored output

--- tests_matrix_oop.cc
+++ tests_matrix_oop.cc
@@ -3,55 +3,37 @@
 #include "Eigen/Dense"
 #include "s21_matrix_oop.h"
 
-#define TOL 1e-01
-
-TEST(Fixture, FirstTest) {
-  int dimension = 2;
-  S21Matrix s21_matrix(dimension, dimension);
-  Eigen::MatrixXd eigen_matrix(dimension, dimension);
-  srand((unsigned) time(NULL));
-  for (int i = 0; i < dimension; i++) {
-    for (int j = 0; j < dimension; j++) {
-      int random = std::rand() % 10;
-      s21_matrix(i, j) = random; 
-      eigen_matrix(i, j) = random; 
-    }
-  }
-  s21_matrix.PrintMatrix();
-  std::cout << std::endl;
-  std::cout << "Eigen determinant: " << eigen_matrix.determinant() << std::endl;
-  std::cout << "S21_matrix determinant: " << s21_matrix.Determinant() << std::endl;
-  ASSERT_TRUE(fabs(s21_matrix.Determinant() - eigen_matrix.determinant()) < TOL);
-}
+#define TOL 1e-06
 
 class MyTestSuite : public testing::TestWithParam<int> {};
 
 TEST_P(MyTestSuite, MyTest)
 {
-  std::cout << "Example Test Param: " << GetParam() << std::endl;
+  std::cout << "Test number: " << GetParam() << std::endl;
   int dimension = GetParam();
   S21Matrix s21_matrix(dimension, dimension);
   Eigen::MatrixXd eigen_matrix(dimension, dimension);
   srand((unsigned) time(NULL));
   for (int i = 0; i < dimension; i++) {
     for (int j = 0; j < dimension; j++) {
-      int random = std::rand() % 10;
+      int random = std::rand() % 2;
       s21_matrix(i, j) = random; 
       eigen_matrix(i, j) = random; 
     }
   }
+  std::cout << "\033[0;34m" << std::endl;
   s21_matrix.PrintMatrix();
-  std::cout << std::endl;
+  std::cout << "\033[0m" << std::endl;
   double result = s21_matrix.Determinant();
   double expected = eigen_matrix.determinant();
   double diff = fabs(result - expected);
-  std::cout << "Eigen determinant: " << expected << std::endl;
+  std::cout << "\033[0;34mEigen determinant: " << expected << std::endl;
   std::cout << "S21_matrix determinant: " << result << std::endl;
-  std::cout << diff << std::endl;
+  std::cout << "Difference: " << diff << "\033[0m" << std::endl;
   ASSERT_TRUE(diff < TOL);
 }
 
-INSTANTIATE_TEST_SUITE_P(MyGroup, MyTestSuite, testing::Range(1, 10),
+INSTANTIATE_TEST_SUITE_P(MyGroup, MyTestSuite, testing::Range(1, 50),
                          testing::PrintToStringParamName());
 
 int main(int argc, char **argv) {
