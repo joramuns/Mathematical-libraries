47708e7421b199580a0062396aea977f66fd4879
Joramun Sasin <joramuns@student.21-school.ru>
2023-03-21 22:39:05 +0300

    tests: experimental tests

--- tests_matrix_oop.cc
+++ tests_matrix_oop.cc
@@ -7,9 +7,8 @@
 
 class MyTestSuite : public testing::TestWithParam<int> {};
 
-TEST_P(MyTestSuite, MyTest)
+TEST_P(MyTestSuite, OneAndZero)
 {
-  std::cout << "Test number: " << GetParam() << std::endl;
   int dimension = GetParam();
   S21Matrix s21_matrix(dimension, dimension);
   Eigen::MatrixXd eigen_matrix(dimension, dimension);
@@ -21,19 +20,34 @@ TEST_P(MyTestSuite, MyTest)
       eigen_matrix(i, j) = random; 
     }
   }
-  std::cout << "\033[0;34m" << std::endl;
-  s21_matrix.PrintMatrix();
-  std::cout << "\033[0m" << std::endl;
   double result = s21_matrix.Determinant();
   double expected = eigen_matrix.determinant();
-  double diff = fabs(result - expected);
-  std::cout << "\033[0;34mEigen determinant: " << expected << std::endl;
-  std::cout << "S21_matrix determinant: " << result << std::endl;
-  std::cout << "Difference: " << diff << "\033[0m" << std::endl;
-  ASSERT_TRUE(diff < TOL);
+  /* ASSERT_TRUE(diff < TOL); */
+  EXPECT_DOUBLE_EQ(expected, result);
+  /* ASSERT_NEAR(result, expected, TOL); */
 }
 
-INSTANTIATE_TEST_SUITE_P(MyGroup, MyTestSuite, testing::Range(1, 50),
+TEST_P(MyTestSuite, OneToTen)
+{
+  int dimension = GetParam();
+  S21Matrix s21_matrix(dimension, dimension);
+  Eigen::MatrixXd eigen_matrix(dimension, dimension);
+  srand((unsigned) time(NULL));
+  for (int i = 0; i < dimension; i++) {
+    for (int j = 0; j < dimension; j++) {
+      int random = 1 + std::rand() % 9;
+      s21_matrix(i, j) = random; 
+      eigen_matrix(i, j) = random; 
+    }
+  }
+  double result = s21_matrix.Determinant();
+  double expected = eigen_matrix.determinant();
+  /* ASSERT_TRUE(diff < TOL); */
+  EXPECT_DOUBLE_EQ(expected, result);
+  /* ASSERT_NEAR(result, expected, TOL); */
+}
+
+INSTANTIATE_TEST_SUITE_P(MyGroup, MyTestSuite, testing::Range(1, 20),
                          testing::PrintToStringParamName());
 
 int main(int argc, char **argv) {
