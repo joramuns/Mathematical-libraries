071a913d26cbd9295910c20c7839723adf37d814
Joramun Sasin <joramuns@student.21-school.ru>
2023-03-22 15:13:37 +0300

    chore: some manual tests

--- main.cc
+++ main.cc
@@ -3,29 +3,49 @@
 #include "s21_matrix_oop.h"
 
 int main() {
-  S21Matrix c(4);
+  S21Matrix c(6);
   /* double content = 9; */
   /* for (int i = 1; i <= 3; i++) { */
   /*   for (int j = 1; j <= 3; j++) { */
   /*     c(i, j) = content++; */
   /*   } */
   /* } */
-  c(0, 0) = 1;
-  c(1, 0) = 1;
-  c(2, 0) = 1;
-  c(3, 0) = 0;
-  c(0, 1) = 1;
-  c(1, 1) = 0;
-  c(2, 1) = 0;
-  c(3, 1) = 0;
-  c(0, 2) = 1;
-  c(1, 2) = 0;
-  c(2, 2) = 0;
+  c(0, 0) = 9;
+  c(1, 0) = 9;
+  c(2, 0) = 8;
+  c(3, 0) = 8;
+  c(4, 0) = 7;
+  c(5, 0) = 9;
+  c(0, 1) = 5;
+  c(1, 1) = 10;
+  c(2, 1) = 2;
+  c(3, 1) = 2;
+  c(4, 1) = 6;
+  c(5, 1) = 6;
+  c(0, 2) = 4;
+  c(1, 2) = 5;
+  c(2, 2) = 9;
   c(3, 2) = 1;
-  c(0, 3) = 1;
-  c(1, 3) = 0;
-  c(2, 3) = 1;
-  c(3, 3) = 0;
+  c(4, 2) = 1;
+  c(5, 2) = 10;
+  c(0, 3) = 3;
+  c(1, 3) = 5;
+  c(2, 3) = 3;
+  c(3, 3) = 10;
+  c(4, 3) = 4;
+  c(5, 3) = 6;
+  c(0, 4) = 5;
+  c(1, 4) = 7;
+  c(2, 4) = 2;
+  c(3, 4) = 3;
+  c(4, 4) = 2;
+  c(5, 4) = 2;
+  c(0, 5) = 5;
+  c(1, 5) = 1;
+  c(2, 5) = 9;
+  c(3, 5) = 2;
+  c(4, 5) = 6;
+  c(5, 5) = 10;
   /* c(0, 0) = 9; */
   /* c(1, 0) = 8; */
   /* c(2, 0) = 7; */
@@ -67,7 +87,6 @@ int main() {
   /* b.PrintMatrix(); */
   std::cout << std::endl << "Matrix C:" << std::endl;
   c.PrintMatrix();
-  std::cout << std::endl << c.null_det();
   /* c.MulMatrix(b); */
   /* std::cout << std::endl << "Matrix C*B:" << std::endl; */
   /* c.PrintMatrix(); */
@@ -79,6 +98,7 @@ int main() {
   /* e.PrintMatrix(); */
   double edet = c.Determinant();
   std::cout << std::endl << " Determinant matrix C test: " << edet << std::endl;
+  std::cout << sizeof(float);
   /* c.PrintMatrix(); */
   /* std::cout << std::endl; */
   /* std::cout << std::endl << "Method check: " << a.EqMatrix(b) << std::endl; */
